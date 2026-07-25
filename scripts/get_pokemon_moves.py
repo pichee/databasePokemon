798c18153778a079166862a71d1a22697affd520
Gustavo <76635390+pichee@users.noreply.github.com>
2026-07-25 11:51:12 -0300

    Feat: Adicionando o script para pegar todos os pokemons que aprendem os ataques

---
+++ scripts/get_pokemon_moves.py
@@ -0,0 +1,20 @@
+# %%
+import pandas as pd
+
+df = pd.read_json("../data/raw/move_pokemon.json")
+
+colums = ['id','learned_by_pokemon']
+df = df[colums]
+
+df = df.explode('learned_by_pokemon')
+df['learned_by_pokemon'] = (
+    df['learned_by_pokemon']
+    .str.get('url')
+    .str.replace("https://pokeapi.co/api/v2/pokemon/", "", regex=False)
+    .str.replace("/","",regex = False))
+
+df = df.dropna(subset=['learned_by_pokemon'])
+df.head()
+df = df.rename(columns={'learned_by_pokemon': 'id_pokemon'})
+
+df.to_csv("../data/processed/move_pokemon.csv",index=False)
