e5285d69f873c72f39bb219602b13ac926a00f09
pichee <gustavopichcavalho@gmail.com>
2026-07-27 18:35:22 -0300

    Conflito resolvido em 03_get_moves.py

--- scripts/05_clean_pokemon_moves.py
+++ scripts/05_clean_pokemon_moves.py
@@ -1,6 +1,10 @@
 # %%
 import pandas as pd
+import os
 
+processed_dir = '../data/processed/'
+
+os.makedirs(processed_dir, exist_ok=True)
 df = pd.read_json("../data/raw/move_pokemon.json")
 
 colums = ['id','learned_by_pokemon']
@@ -16,5 +20,6 @@ df['learned_by_pokemon'] = (
 df = df.dropna(subset=['learned_by_pokemon'])
 df.head()
 df = df.rename(columns={'learned_by_pokemon': 'id_pokemon'})
+
 
 df.to_csv("../data/processed/move_pokemon.csv",index=False)
